e5cf555f2f508443d9acfc70aaf1e1fe9241fad1
goatdjar <jar.padul@pm.me>
2026-01-15 13:41:26 +0100

    Add nix flake development environment

---
+++ flake.nix
@@ -0,0 +1,46 @@
+# in flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell rec {
+            name = "Sandbox Development Environment";
+            packages = with pkgs; [
+              python312
+              python312Packages.python-lsp-ruff
+              python312Packages.python-lsp-server
+
+              uv
+
+              gh
+
+              # ruff-lsp
+              # rust-analyzer
+            ];
+
+            buildInputs = [
+              # rust-bin.stable.latest.default
+            ];
+
+            shellHook = ''
+              export PIP_PREFIX="$PWD/_build/pip_packages"
+              export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+              export PATH="$PIP_PREFIX/bin:$PATH"
+            '';
+
+          };
+        }
+      );
+}
