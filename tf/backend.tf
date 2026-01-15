6ab6fa6f57e5e6464d16265cb7342a7c6a6ce9ba
goatdjar <jarpadul@gmail.com>
2026-01-15 17:40:52 +0100

    Test 5

--- tf/backend.tf
+++ tf/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "aws-s3-tfstate-test-1122334455"
-    key     = "terraform/state"
-    region  = "ap-southeast-1"
-    encrypt = true
+    bucket        = "aws-s3-tfstate-test-1122334455"
+    key           = "terraform/state"
+    region        = "ap-southeast-1"
+    encrypt       = true
+    force_destroy = true
   }
 }
